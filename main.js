119373c663e26e5826615aab6284b9115ccdc5ca
MC <taliaferromc@gmail.com>
2017-12-19 14:49:28 +0200

    Debugged version

--- main.js
+++ main.js
@@ -8,16 +8,18 @@ var name = prompt('Please enter the name for your reservation');
 var claimReservation = function() {
   // write your code here!
   for (var input in reservations) {
-    if (input === name && reservations[input].claimed === false) {
+    if (input.toUpperCase() === name.toUpperCase() && reservations[input].claimed === false) {
       alert('Welcome!');
+      return;
     }
-    if (input === name && reservations[input].claimed === true) {
+    if (input.toUpperCase() === name.toUpperCase() && reservations[input].claimed === true) {
       alert('Welcome! Your party is already waiting.');
+      return;
     }
-    if (input !== name) {
+  } 
       alert('There is no reservation with this name');
-    }
-  }
+      reservations[name] = { claimed: true };
+      return; 
 };
 
 claimReservation();
