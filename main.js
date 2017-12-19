4a470e56111f4c5f265859e7803e8b91492ce374
MC <taliaferromc@gmail.com>
2017-12-19 11:56:22 +0200

    Updated solutions

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 var reservations = {
   'Bob': { claimed: false },
-  'Ted': { claimed: true }
-}
+  'Ted': { claimed: true },
+};
 
 var name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
+var claimReservation = function() {
   // write your code here!
-}
+  for (var input in reservations) {
+    if (input === name && reservations[input].claimed === false) {
+      alert('Welcome!');
+    }
+    if (input === name && reservations[input].claimed === true) {
+      alert('Welcome! Your party is already waiting.');
+    }
+    if (input !== name) {
+      alert('There is no reservation with this name');
+    }
+  }
+};
 
 claimReservation();
